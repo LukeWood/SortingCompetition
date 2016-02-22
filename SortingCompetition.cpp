ef3aa5b7e3ff807f73858a09a7d036c4e6fa0720
Luke Wood <lswood@smu.edu>
2016-02-22 15:21:26 -0600

    Added base files

---
+++ SortingCompetition.cpp
@@ -0,0 +1,54 @@
+#include "SortingCompetition.h"
+#include <fstream>
+#include <iostream>
+
+//Pretty simple constructor, does exactly what you'd expect.
+SortingCompetition::SortingCompetition(const string& inputFileName)
+{
+	words = NULL;
+	fname = inputFileName;
+}
+
+//Changes the filename
+void SortingCompetition::setFileName(const string& inputFileName)
+{
+	fname = inputFileName;
+}
+
+//Reads data in.
+bool SortingCompetition::readData()
+{
+	std::ifstream in(fname.c_str());
+	string temp;
+	if(words)
+	{
+		deleteWords();
+	}
+	
+	in>>numwords;
+	words = new char*[numwords];
+	for(int i = 0; i < numwords; i++)
+	{
+		in>>temp;
+		int len = temp.length();
+		words[i] = new char[len+1];
+		for(int j = 0; j <len; j++)
+		{
+			words[i][j] = temp[j]; 
+		}
+		words[i][len] = '\0';
+	}
+	in.close();
+
+	return true;
+}
+
+void SortingCompetition::deleteWords()
+{
+	for(int i = 0; i < numwords; i++)
+	{
+		delete[] words[i];
+	}
+	delete[] words;
+}
+
