e4b8611adfaa300cc7cf69588ac570e16727218c
Luke Wood <lswood@smu.edu>
2016-02-23 15:42:40 -0600

    Work

--- testfilegen/filegen.py
+++ testfilegen/filegen.py
@@ -2,12 +2,13 @@ import sys
 
 with open(sys.argv[1], "w+") as f:
 	for i in range(0,int(sys.argv[2])):
-		chartowrite = (i % 221)+34
+		chartowrite = (i % 221)+32
 		length = int(i/10)
 		towrite = ""
 		for j in range(0, length + 1):
-			nextc = (((chartowrite+j)%221)+34)
+			nextc = (((chartowrite+j)%221)+32)
 			if(nextc < 255 and nextc > 0):
-				towrite+=str(chr(nextc))
+				towrite = towrite + str(chr(nextc))
+		print(towrite)
 		f.write(towrite + '\n')
 	f.close()
